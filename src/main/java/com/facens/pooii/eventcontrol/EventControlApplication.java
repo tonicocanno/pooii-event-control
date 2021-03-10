ba21c5357fcda97f6db05bc115467c0ca94f9ad8
Antonio <antonio.canno257@gmail.com>
2021-03-09 21:43:16 -0300

    first commit

---
+++ src/main/java/com/facens/pooii/eventcontrol/EventControlApplication.java
@@ -0,0 +1,13 @@
+package com.facens.pooii.eventcontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventControlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/facens/pooii/eventcontrol/EventControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.facens.pooii.eventcontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
