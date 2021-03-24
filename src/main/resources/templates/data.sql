95c7d3613acf35481ab4cb38af8ed1dd258920ad
Antonio <antonio.canno257@gmail.com>
2021-03-23 21:41:17 -0300

    Heroku update

--- src/main/resources/templates/data.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO tb_client (name, description, email) 
-VALUES (
-    'João Antonio', 'Start-Up de Tecnologia', 'suporte@devevo.com.br'
-    );
-
-INSERT INTO tb_client (name, address) 
-VALUES (
-    'Maria Silva', 'Corrida de F1 Kids', 'contato@formula.com.br'
-    );
